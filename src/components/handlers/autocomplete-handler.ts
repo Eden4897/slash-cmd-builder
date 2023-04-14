10fd1e5662dfd50a116f995ee95b977267afbc6d
Simon <44496148+siim0n@users.noreply.github.com>
2023-04-14 20:50:39 +0200

    should fix autocomplete

--- src/components/handlers/autocomplete-handler.ts
+++ src/components/handlers/autocomplete-handler.ts
@@ -9,21 +9,27 @@ export async function handleAutocomplete(interaction: AutocompleteInteraction) {
 
   const subcommandGroupName = interaction.options.getSubcommandGroup(false);
   if (subcommandGroupName) {
-      const subcommandGroup = command.subcommandGroups.find(subcommandGroup => subcommandGroup.data.name == subcommandGroupName);
-      const subcommandName = interaction.options.getSubcommand();
-      if (subcommandName) {
-          const subcommand = subcommandGroup.subcommands.find(subcommand => subcommand.data.name == subcommandName);
-          if (subcommand.autocompleter)
-              return await interaction.respond((await subcommand.autocompleter(interaction)).map(choice => ({ name: choice, value: choice })));
+    const subcommandGroup = command.subcommandGroups.find(subcommandGroup => subcommandGroup.data.name == subcommandGroupName);
+    const subcommandName = interaction.options.getSubcommand();
+    if (subcommandName) {
+      const subcommand = subcommandGroup.subcommands.find(subcommand => subcommand.data.name == subcommandName);
+      if (subcommand.autocompleter) {
+        const choices = (await subcommand.autocompleter(interaction)).map(choice => ({ name: choice, value: choice })).splice(0, 25);
+        return await interaction.respond(choices);
       }
+    }
   }
   // If it is a subcommand
   const subcommandName = interaction.options.getSubcommand(false);
   if (subcommandName) {
-      const subcommand = command.subcommands.find(subcommand => subcommand.data.name == subcommandName);
-      if (subcommand.autocompleter)
-          return await interaction.respond((await subcommand.autocompleter(interaction)).map(choice => ({ name: choice, value: choice })));
+    const subcommand = command.subcommands.find(subcommand => subcommand.data.name == subcommandName);
+    if (subcommand.autocompleter) {
+      const choices = (await subcommand.autocompleter(interaction)).map(choice => ({ name: choice, value: choice })).splice(0, 25);
+      return await interaction.respond(choices);
+    }
   }
-  if (command.autocompleter)
-      return await interaction.respond((await command.autocompleter(interaction)).map(choice => ({ name: choice, value: choice })));
+  if (command.autocompleter) {
+    const choices = (await command.autocompleter(interaction)).map(choice => ({ name: choice, value: choice })).splice(0, 25);
+    return await interaction.respond(choices);
+  }
 }
