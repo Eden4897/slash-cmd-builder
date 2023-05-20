60ad16705e6e99f141711e6d6322d0bb6f868077
Simon <44496148+siim0n@users.noreply.github.com>
2023-05-20 18:41:53 +0200

    Update command-handler.ts
    try catch getSubcommand and getSubcommandgroup

--- src/components/handlers/command-handler.ts
+++ src/components/handlers/command-handler.ts
@@ -2,32 +2,52 @@ import { CommandInteraction } from "discord.js";
 import { commands } from "../builder";
 
 export async function handleCommand(interaction: CommandInteraction) {
-	const command = commands.get(interaction.commandName);
+    const command = commands.get(interaction.commandName);
 
-	if (!command) return;
+    if (!command) return;
 
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		await interaction
-			.reply({
-				content: 'There was an error while executing this command!',
-				ephemeral: true,
-			})
-			.catch(() =>
-				interaction.followUp({
-					content: 'There was an error while executing this command!',
-					ephemeral: true,
-				})
-			);
-			const subGroup = interaction.options.getSubcommandGroup();
-			const subCommand = interaction.options.getSubcommand();
-			if (subGroup) {
-				console.error(`Error while executing "${interaction.commandName}" command in subcommand group "${subGroup}":`, error);
-			} else if (subCommand) {
-				console.error(`Error while executing "${interaction.commandName}" command in subcommand "${subCommand}":`, error);
-			} else {
-				console.error(`Error while executing "${interaction.commandName}" command:`, error);
-			}
-	}
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        let subCommand: string | undefined = undefined;
+        let subCommandGroup: string | undefined = undefined;
+
+        // Try to fetch subcommand group, if it exists
+        if (interaction.options.data.find((option) => option.type === 'SUB_COMMAND_GROUP')) {
+            try {
+                subCommandGroup = interaction.options.getSubcommandGroup();
+            } catch (e) {
+                subCommandGroup = undefined;
+            }
+        }
+
+        // Try to fetch subcommand, if it exists
+        if (interaction.options.data.find((option) => option.type === 'SUB_COMMAND')) {
+            try {
+                subCommand = interaction.options.getSubcommand();
+            } catch (e) {
+                subCommand = undefined;
+            }
+        }
+
+        await interaction
+            .reply({
+                content: 'There was an error while executing this command!',
+                ephemeral: true,
+            })
+            .catch(() =>
+                interaction.followUp({
+                    content: 'There was an error while executing this command!',
+                    ephemeral: true,
+                })
+            );
+
+        if (subCommandGroup) {
+            console.error(`Error while executing "${interaction.commandName}" command in subcommand group "${subCommandGroup}":`, error);
+        } else if (subCommand) {
+            console.error(`Error while executing "${interaction.commandName}" command in subcommand "${subCommand}":`, error);
+        } else {
+            console.error(`Error while executing "${interaction.commandName}" command:`, error);
+        }
+    }
 }
